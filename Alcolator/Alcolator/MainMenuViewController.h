71b2ab23768f1be32aeb7242b5a62b30ecb3edc1
Dean Laurea <deanlaurea@gmail.com>
2015-06-05 18:59:24 -0500

    Checkpoint 20 completed, basic navigation

---
+++ Alcolator/Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Dean Laurea on 6/5/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MainMenuViewController : ViewController
+
+@end
